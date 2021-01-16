57d77f848d81a9447010e48cdc2572152bb7ed41
Hope <xhforeman@outlook.com>
2021-01-16 12:39:52 -0500

    Hangman Project

---
+++ src/hangman/HangmanApp.java
@@ -0,0 +1,12 @@
+package hangman;
+
+public class HangmanApp {
+
+    public static void main(String[] args) {
+        Hangman game = new Hangman();
+        game.run();
+        game.close();
+
+    }
+
+}
---
+++ src/hangman/RandomWord.java
@@ -0,0 +1,45 @@
+package hangman;
+
+import java.util.Random;
+
+public class RandomWord {
+    private String[] words = {"father", "family", "tombstone", "firefighter", "dexter", "gucci", "photographs"};
+    private String chosenWord;
+    private Random random = new Random();
+    private char[] characters;
+
+    public RandomWord(){
+        chosenWord = words[random.nextInt(words.length)];
+        characters = new char[chosenWord.length()];
+    }
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for(char c: characters) {
+            sb.append(c == '\u0000' ? '_': c);
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    public boolean isComplete(){
+        for(char c: characters){
+            if(c == '\u0000'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean addGuess(char c){
+        // Fill in c in the character array wherever it's found in the random word
+        boolean correct = false;
+        for(int i=0; i<chosenWord.length(); i++){
+            if(c == chosenWord.charAt(i)){
+                characters[i] = c;
+                correct = true;
+            }
+        }
+        return correct;
+    }
+}
